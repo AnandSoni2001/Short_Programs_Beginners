b03eed77c0323303f345c2ffb6a9c0485df65f79
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2021-01-29 20:12:18 +0530

    Update Bubble Sort.c

--- Bubble Sort.c
+++ Bubble Sort.c
@@ -9,14 +9,14 @@ Example:
 Let an array be of length 5 with values :
 9 2 5 1 7
 
- INITIAL		        SWAPPING            FINAL
+ INITIAL		   SWAPPING        FINAL
 
-9 2 5 1 7	     i=0, j=0, j+1=1		    2 9 5 1 7
-2 9 5 1 7	     i=0, j=1, j+1=2		    2 5 9 1 7                                                                                                                                                          
-2 5 9 1 7      i=0, j=2, j+1=3	     2 5 1 9 7                                                                                                                                                   
-2 5 1 9 7      i=0, j=3, j+1=4	     2 5 1 7 9                                                                                                                                                   
-2 5 1 7 9      i=1, j=1, j+1=2	     2 1 5 7 9                                                                                                                                                   
-2 1 5 7 9      i=2, j=0, j+1=1      1 2 5 7 9 (Ans)                                                                                                                                           
+9 2 5 1 7	i=0, j=0, j+1=1		2 9 5 1 7
+2 9 5 1 7	i=0, j=1, j+1=2		2 5 9 1 7                                                                                                                                                          
+2 5 9 1 7	i=0, j=2, j+1=3		2 5 1 9 7                                                                                                                                                   
+2 5 1 9 7	i=0, j=3, j+1=4		2 5 1 7 9                                                                                                                                                   
+2 5 1 7 9	i=1, j=1, j+1=2		2 1 5 7 9                                                                                                                                                   
+2 1 5 7 9	i=2, j=0, j+1=1		1 2 5 7 9 (Ans)                                                                                                                                           
 
 Take a moment to make an algorithm yourself, it helps a lot, otherwise the solution 
 is just below, Enjoy Coding !!!
